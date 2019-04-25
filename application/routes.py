1a210efc224c4319da3c46f10fe30498ddf93a77
Teresa Ibarra <mtmibarra@gmail.com>
2019-04-25 14:36:00 -0700

    Check for both song name and artist

--- application/routes.py
+++ application/routes.py
@@ -9,6 +9,9 @@ def lookup():
     form = LoginForm()
     if form.validate_on_submit():
         return redirect('/recommendations/' + form.artist.data + '/' +  form.title.data)
+    elif (form.artist.data and not form.title.data) or (not form.artist.data and form.title.data):
+        flash('Whoops! Please enter both the song name and artist.', category='error')
+        return render_template('whoops.html', title='Input error')
     return render_template('lookup.html', title='Smarter Music Recommendations', form=form)
 
 @app.route('/about')
