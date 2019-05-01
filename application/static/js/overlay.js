b585e9187b2ef797654d4f70c4bcbf7c395974a5
Teresa Ibarra <mtmibarra@gmail.com>
2019-04-30 21:28:51 -0700

    Stop music when out of overlay

--- application/static/js/overlay.js
+++ application/static/js/overlay.js
@@ -6,5 +6,9 @@ function on(id) {
 
 function off() {
   document.getElementById("full-overlay").style.display = "none";
+  $('audio').each(function(){
+  		this.pause(); // Stop playing
+  			this.currentTime = 0; // Reset time
+  		}); 
   $('div[id^="div-"]').hide();
 }
